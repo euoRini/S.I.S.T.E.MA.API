7267ffe285c64b33650ad99c9f69a7b6b1cf2201
GustavoBazinga <63375206+GustavoBazinga@users.noreply.github.com>
2020-04-11 00:34:44 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ const { Model, DataTypes } = require('sequelize');
 class User extends Model {
   static init(connection){
     super.init({
-      matricula: DataTypes.string,
+      matricula: DataTypes.STRING,
       nome: DataTypes.STRING,
       email: DataTypes.STRING,
       saldo: DataTypes.INTEGER,
