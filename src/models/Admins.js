d9cf07714255f2717d48646a7e52c65fae0e23f2
GustavoBazinga <63375206+GustavoBazinga@users.noreply.github.com>
2020-04-10 21:31:46 -0300

    Update Admins.js

--- src/models/Admins.js
+++ src/models/Admins.js
@@ -3,10 +3,10 @@ const { Model, DataTypes } = require('sequelize');
 class Admins extends Model {
     static init(connection) {
       super.init({
-        nome: DataTypes.STRING(50),
-        login: DataTypes.STRING(50),
-        senha: DataTypes.STRING(100),
-        email: DataTypes.STRING(50),
+        nome: DataTypes.STRING,
+        login: DataTypes.STRING,
+        senha: DataTypes.STRING,
+        email: DataTypes.STRING,
     },  {
             sequelize: connection
         })
