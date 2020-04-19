ba9905078eee86b99cd07b046e1398bbd257e81e
euoRini <61603802+euoRini@users.noreply.github.com>
2020-04-19 20:54:29 -0300

    login

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ const AcessoController = require ('./controllers/AcessoController');
 const ProdutoController = require ('./controllers/ProdutoController');
 
 const routes = express.Router();
-router.use(authMiddleware);
+routes.use(authMiddleware);
 routes.get('/',(req,res)=>{ res.send('Sistema  em funcionamento...'); });
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
