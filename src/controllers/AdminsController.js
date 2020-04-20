416bbeb8880471f616039b285b5602907c2f69b6
euoRini <61603802+euoRini@users.noreply.github.com>
2020-04-19 21:00:49 -0300

    login

--- src/controllers/AdminsController.js
+++ src/controllers/AdminsController.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth.json')
 
-
+routes.use(authMiddleware);
 function generateToken(params = {}){
   return jwt.sign(params, authConfig.secret,{
     expiresIn: 86400,
