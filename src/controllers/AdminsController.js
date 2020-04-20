99c9e8b4746650774da56b2b6d14fffbb4b5b3c2
euoRini <61603802+euoRini@users.noreply.github.com>
2020-04-20 15:22:04 -0300

    login

--- src/controllers/AdminsController.js
+++ src/controllers/AdminsController.js
@@ -67,7 +67,7 @@ module.exports = {
     const { nome, login, crpsenha, email } = req.body;
     const senha = await bcrypt.hash(crpsenha, 10);
     const admin = await Admins.create({nome, login, senha, email});
-    return res.status(200).send({token: generateToken({id: admin.id})});
+    return res.status(200).send(admin);
   },
   
   async login(req, res){
