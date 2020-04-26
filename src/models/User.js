89e0b6f65ce7fc1d89a98468b5b4cedc54ebfe96
euoRini <61603802+euoRini@users.noreply.github.com>
2020-04-25 21:15:51 -0300

    correções

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ const { Model, DataTypes } = require('sequelize');
 class User extends Model {
   static init(connection){
     super.init({
-      matricula: DataTypes.INTEGER,
+      matricula: DataTypes.STRING,
       nome: DataTypes.STRING,
       email: DataTypes.STRING,
       saldo: DataTypes.INTEGER,
--- src/models/Vendedor.js
+++ src/models/Vendedor.js
@@ -3,10 +3,10 @@ const { Model, DataTypes } = require('sequelize');
 class Vendedor extends Model {
     static init(connection) {
       super.init({
-        matricula: DataTypes.STRING(50),
-        nome: DataTypes.STRING(50),
-        email: DataTypes.STRING(50),
-        senha: DataTypes.STRING(100),
+        matricula: DataTypes.STRING,
+        nome: DataTypes.STRING,
+        email: DataTypes.STRING,
+        senha: DataTypes.STRING,
     },  {
             sequelize: connection   
         })
