bd2fd8c14d764b255180568d231c0986c5ff7038
euoRini <61603802+euoRini@users.noreply.github.com>
2020-07-04 21:20:47 -0300

    new database

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'mysql',
-  host: 'remotemysql.com',
-  username: 'fO570IdCqW',
-  password: 'StZTV2T0wU',
-  database: 'fO570IdCqW',
+  host: 'sql10.freemysqlhosting.net',
+  username: 'sql10352688',
+  password: 'hkiMhDjG7l',
+  database: 'sql10352688',
   define: {
     timestamps: true,
     underscored: true,
