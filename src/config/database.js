31c9c3d730c2fdd99d0e7ec97cec8b31a21b3141
euoRini <61603802+euoRini@users.noreply.github.com>
2020-08-10 17:49:15 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'mysql',
-  host: 'databases-auth.000webhost.com',
-  username: 'id14274215_root',
-  password: '<rP!%1_fN5EYc$h8',
-  database: 'id14274215_sistematcc',
+  host: 'localhost',
+  username: 'UxvPrla7rY',
+  password: 'Qf5ni4XuKM',
+  database: 'UxvPrla7rY',
   define: {
     timestamps: true,
     underscored: true,
