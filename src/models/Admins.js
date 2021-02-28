8f2bf9c38a87ce98198f8f645fa249530417c67f
euoRini <61603802+euoRini@users.noreply.github.com>
2021-02-28 18:57:56 -0300

    .

--- src/models/Admins.js
+++ src/models/Admins.js
@@ -12,7 +12,7 @@ class Admins extends Model {
         })
     }
     static associate(models) {
-        this.belongsToMany(models.Acesso, {foreignKey: 'id_admin', as: 'ADMacessos'});
+        this.hasMany(models.Acesso, {foreignKey: 'id_admin', as: 'ADMacessos'});
     }
 }
 
