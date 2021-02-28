94cc21c5221977b24cfda9d2a95f4249d1207b42
euoRini <61603802+euoRini@users.noreply.github.com>
2021-02-28 16:50:02 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'mysql',
-  host: 'db4free.net',
-  username: 'sistema_adm',
-  password: 'km**1a!Q(1o3xYKK5#BX',
-  database: 'tcc_sistema_bd',
+  host: 'remotemysql.com',
+  username: 'WnHanH0DXp',
+  password: 'vGkRBPpixt',
+  database: 'WnHanH0DXp',
   define: {
     timestamps: true,
     underscored: true,
