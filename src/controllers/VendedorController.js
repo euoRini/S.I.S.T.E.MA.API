00d04da4d64c7f4ef04de03cf617b53b5f605066
euoRini <61603802+euoRini@users.noreply.github.com>
2021-05-01 16:57:43 -0300

    .

--- src/controllers/VendedorController.js
+++ src/controllers/VendedorController.js
@@ -50,12 +50,11 @@ module.exports = {
 
   async update(req,res)
   {
-    const {matricula} = req.params;
-    const crpsenha = req.body.crpsenha;
-    const senha = await bcrypt.hash(crpsenha, 10);
-    const vendedor = await Vendedor.update({ nome : req.body.nome, matricula: req.body.matricula, email: req.body.email, senha:senha},{where:{matricula:matricula}});
-    return res.status(200).json(vendedor);
-
+    // const {matricula} = req.params;
+    // const crpsenha = req.body.crpsenha;
+    // const senha = await bcrypt.hash(crpsenha, 10);
+    // const vendedor = await Vendedor.update({ nome : req.body.nome, matricula: req.body.matricula, email: req.body.email, senha:senha},{where:{matricula:matricula}});
+    return res.status(200).json("Batata grande");
   },
 
   async login(req,res){
